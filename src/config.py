7561ec9244845c777198dd1e0a3686bb0c170932
michelfaria <michelfariarj@gmail.com>
2018-01-19 01:34:21 -0200

    Override

--- src/config.py
+++ src/config.py
@@ -1,4 +1,5 @@
 import yaml
+import logging
 
 
 class SeeBorg4Config:
@@ -7,6 +8,7 @@ class SeeBorg4Config:
         """
         :param dict_: ``dict``
         """
+        self._logger = logging.getLogger(SeeBorg4Config.__name__)
         self._dict = dict_
 
     @staticmethod
@@ -19,7 +21,45 @@ class SeeBorg4Config:
             return SeeBorg4Config(yaml.load(f.read()))
 
     def token(self):
+        """
+        :return: ``str``
+        """
         return self._dict['token']
 
     def database_path(self):
+        """
+        :return: ``str``
+        """
         return self._dict['databasePath']
+
+    def is_ignored(self, author_id, channel_id):
+        """
+        :param author_id: ``str``
+        :param channel_id: ``str``
+        :return: ``bool``
+        """
+        return author_id in self.behavior(channel_id, 'ignoredUsers')
+
+    def behavior(self, channel_id, property_name):
+        """
+        Returns the property for the given channel if it's overridden; otherwise, it returns the
+        property from the default, root behavior.
+
+        :param channel_id: ``str``
+        :param property_name: ``str``
+        :return: ``any``
+        """
+        default_behavior_value = self._dict['behavior'][property_name]
+        override = self._override_for_channel(channel_id)
+
+        if override is None or property_name not in override['behavior']:
+            return default_behavior_value
+        else:
+            self._logger.debug('OVERRIDDEN BEHAVIOR %s IN %s' % (property_name, channel_id))
+            return override['behavior'][property_name]
+
+    def _override_for_channel(self, channel_id):
+        for override in self._dict['channelOverrides']:
+            if override['channelId'] == channel_id:
+                return override
+        return None
--- src/seeborg4.py
+++ src/seeborg4.py
@@ -26,9 +26,27 @@ class SeeBorg4:
             """
             :param message: ``discord.Message``
             """
-            self._logger.info('MSG %s: %s' % (message.channel.id, message.content))
+            self._logger.info('MSG %s %s: %s' % (message.channel.id, message.author.id, message.content))
+
+            # Ignore own messages
+            if self._is_own_message(message):
+                return
+
+            if self._config.is_ignored(message.author.id, message.channel.id):
+                self._logger.debug('IGNORE %s' % message.author.id)
+                return
+
+            self._logger.info('Woohoo!')
 
     def start(self):
         self._logger.info('SeeBorg4 is starting')
         self._register_listeners()
         self._client.run(self._config.token())
+
+    def _is_own_message(self, message):
+        """
+        Returns true if the specified message belongs to our `_client`.
+        :param message: ``discord.Message``
+        :return: ``bool``
+        """
+        return message.author.id == self._client.user.id
